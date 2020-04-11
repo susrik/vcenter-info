4fcfb32554eea9055c330d57cf9573f9924b7702
Erik Reid <erik.reid@geant.org>
2020-04-11 11:41:45 +0200

    added setup config

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='vcenter-info',
+    version="0.0.1",
+    author='Erik Reid',
+    author_email='nobody@nowhere.org',
+    description='Simple view of vCenter data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/susrik/vcenter-info',
+    packages=find_packages(),
+    install_requires=[
+        'pyvmomi',
+        'jinja2',
+        'jsonschema',
+        'flask'
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta'
+    ],
+    python_requires='>=3.5'
+)
+
