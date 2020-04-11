2e1ef39f2f8c0236e6a5313ebee8996bdfb57cfd
Erik Reid <erik.reid@geant.org>
2020-04-11 08:32:28 +0200

    skeleton app

---
+++ webapp/jsx/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import VMQueryResults from './VMQueryResults';
+
+ReactDOM.render(<VMQueryResults/>, document.getElementById('main'));
+
